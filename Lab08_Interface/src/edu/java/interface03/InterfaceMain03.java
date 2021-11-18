c46c14691be504276ab3067078f0725bed0a8a03
knit <knit@DESKTOP-OC8SF7D>
2021-11-18 14:59:03 +0900

    .

--- Lab08_Interface/src/edu/java/interface03/InterfaceMain03.java
+++ Lab08_Interface/src/edu/java/interface03/InterfaceMain03.java
@@ -7,4 +7,9 @@ public class InterfaceMain03 {
 
 	}
 
-}
+}  // end class InterfaceMain03
+// 상위 클래스 상속(extends)과 인터페이스 구현(implements)를 동시에 하는 경우 :
+class SuperClass {}
+interface MyInterface {}
+class SubClass extends SuperClass
+		implements MyInterface{}
