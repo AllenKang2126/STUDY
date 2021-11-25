50ac4bed2ae24d00e2dda858aa86360f10e27a81
KBJ <KBJ@14.52.89.84>
2021-11-25 13:41:52 +0900

    .

--- Lab13_Collection/src/edu/java/list03/ListMain03.java
+++ Lab13_Collection/src/edu/java/list03/ListMain03.java
@@ -1,7 +1,7 @@
 package edu.java.list03;
 
-import java.io.ObjectInputStream.GetField;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ListMain03 {
@@ -9,19 +9,42 @@ public class ListMain03 {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		// 기본 생성자(default constructor)를 사용해서 Member 클래스의 인스턴스를 생성
+		Member m1 = new Member();	// 쌤 코드
+		System.out.println("m1 : " + m1);  // 쌤 코드 - toString 메서드를 override하기 전/후의 결과 비교
+		// getter : 멤버 변수(필드)의 값을 가져옴(읽어옴.)
+		System.out.println("m1.memberId : " + m1.getMemberId());  // 쌤 코드
+		System.out.println("m1.memberPassword : " + m1.getMemberPassword());  // 쌤 코드
+		// setter : 멤버 변수(필드)의 값 설정.
+		m1.setMemberPassword("abcd");  // 쌤 코드
+		System.out.println("m1.memberPassword : " + m1.getMemberPassword());  // 쌤 코드
+		
+		// 파라미터를 갖는 생성자를 사용해서 Member 클래스의 인스턴스를 생성.
+		Member m2 = new Member("할머니", "안녕하세요");  // 쌤 코드
+		System.out.println("m2 : " + m2);  // 쌤 코드
+		
+		System.out.println();
+		
 		int count = 0;
 		// Member 타입을 저장할 수 있는 ArrayList를 생성
-		ArrayList<Member> list = new ArrayList<Member>();
+		List<Member> list = new ArrayList<Member>();  // 생성자를 호출할 때는 ArrayList가 저장하는 타입을 생략할 수 있음.
+		System.out.println(list);
+		
 		// 반복문을 사용해서
 		// 아이디와 비밀 번호를 입력받아서 Member 객체를 생성해서
 		// ArrayList에 Member 타입 객체 3개를 추가
 		for (int i = 0; i < 3; i++) {
+			// id / password를 입력 받음.
 			System.out.println("아이디를 입력해주세요");
-			String s1 = scanner.nextLine();
+			String id = scanner.nextLine();
 			System.out.println("비밀번호를  입력해주세요");
-			String s2 = scanner.nextLine();
-			Member member = new Member(s1, s2);
+			String pw = scanner.nextLine();
+			// Member 객체 생성
+			Member member = new Member(id, pw);
+			// Member 객체를 ArrayList에 추가
 			list.add(member);
+			// 회원 정보가 추가된 내용을 출력.
+			System.out.println(list);
 		}
 
 //		Member member = new Member("123","123");
@@ -29,7 +52,10 @@ public class ListMain03 {
 //		System.out.println(list.get(0).getMemberId());
 //		System.out.println(list.get(0).getMemberPassword());
 
-		// 리스트의 내용을 출력 - 각각의 회원의 아이디와 비밀번호가 출력될 수 있도록
+		// 리스트의 내용을 출력 - 각각의 회원의 아이디와 비밀번호가 출력될 수 있도록(toString override test)
+		for(Member m : list) {  // 쌤 코드
+			System.out.println(m);  // 쌤 코드
+		}
 		for (int i = 0; i < 3; i++) {
 			System.out.println("ID : " + list.get(i).getMemberId() + ", PW : " + list.get(i).getMemberPassword());
 		}
@@ -37,28 +63,42 @@ public class ListMain03 {
 		// 인덱스 0 위치의 회원 정보 중에서 비밀번호를 변경하고 리스트 내용 출력
 		System.out.println("변경할 비밀번호를 입력해주세요");
 		String s = scanner.nextLine();
-		Member m = list.get(0);
-		m.setMemberPassword(s);
-
-		for (int i = 0; i < 3; i++) {
+		Member m = list.get(0);  // (1) 인덱스 0 위치의 회원 정보를 찾음.
+		m.setMemberPassword(s);  // (2) 그 회원의 비번을 설정(setter).
+		// list.get(0).setMemberPassword("ABCD");  // 메서드 연쇄 호출(chain call)
+		System.out.println(list);
+		System.out.println();
+		/*for (int i = 0; i < 3; i++) {
 			System.out.println("ID : " + list.get(i).getMemberId() + ", PW : " + list.get(i).getMemberPassword());
 		}
-		System.out.println();
+		System.out.println();*/  // 내 코드
 
 		// Member m = memberlist.get(0); m.setPwd(새 비번);
 		// (1) 인덱스 0 위치의 회원정보를 찾음. (2) 그 회원의 비번을 설정(setter)
 
 		// 인덱스 1 위치의 회원 정보를 삭제하고 리스트 내용을 출력. - list.remove(int index)
-		
+		list.remove(1);
+		System.out.println(list);  // 쌤 코드
+		System.out.println();
 
-		for (int i = 0; i < 3; i++) {
+		/*for (int i = 0; i < 3; i++) {
 			System.out.println("ID : " + list.get(i).getMemberId() + ", PW : " + list.get(i).getMemberPassword());
 		}
-		System.out.println();
+		System.out.println();*/ // 내코드
 
 		// 아이디가 일치하면 같은 회원으로 간주되도록 Member 클래스를 수정. - equals, hashCode
 		// 아이디가 일치하는 회원을 리스트에서 삭제하고 리스트 내용을 출력. - list.remove(Object value)
-
+		Member member = new Member("AAA", "@@@");
+		/*for(int i = 0; i < list.size(); i++) {  // 인덱스 0부터 마지막 원소까지 반복하면서
+			Member m3 = list.get(i);  // 인덱스 위치에 있는 원소를 가져옴.
+			if(m3.getMemberId().equals(member.getMemberId())) {
+				// 리스트에 저장된 회원 아이디와 삭제하려고 하는 회원 아이디가 같다면
+				list.remove(i);  // 리스트에서 그 인덱스의 원소를 삭제.
+				break;  // 반복문 종료
+			}
+		}*/
+		list.remove(member);
+		System.out.println(list);
 	}
 
 }
--- Lab13_Collection/src/edu/java/list03/Member.java
+++ Lab13_Collection/src/edu/java/list03/Member.java
@@ -4,7 +4,7 @@ public class Member {
 	// field
 	private String memberId;
 	private String memberPassword;
-	// TODO : 생성자 - 기본 생성자, 파라미터 2개인 생성자
+	// 생성자(Constructor) - 기본 생성자, 파라미터 2개인 생성자
 	public Member() {}
 	
 	public Member(String memberId, String memberPassword) {
@@ -13,7 +13,7 @@ public class Member {
 	}
 
 	
-	// TODO : getters & setters
+	// getters & setters(필드가 가지고 있는 값을 리턴) & setters(필드의 값을 변경)
 	public String getMemberId() {
 		return memberId;
 	}
@@ -33,4 +33,29 @@ public class Member {
 	
 	
 	// toString override
+	@Override
+	public String toString() {
+		return "Member{memberId : " + this.memberId + ", memberPassWord : " + this.memberPassword + "}";
+	}
+	
+	// equals 메서드 override
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
+		if(obj instanceof Member) {  // 파라미터 obj가 Member 타입 객체이면
+			Member m = (Member) obj;  // casting(강제 타입 변환)
+			if(this.memberId.equals(m.memberId)) {  // 나의 memberId와 파라미터로 전달된 객체의 memberId가 같으면
+				result = true;
+			}
+		}
+		return result;
+	}
+	
+	// hashCode 메서드 override : equals가 true를 리턴하는 두 객체는 같은 hashCode값을 갖도록 구현
+	@Override
+	public int hashCode() {
+		// equals를 override할 떄 memberId만 같으면 true가 되도록 구현했기 때문에,
+		// hashCode도 memberId(String 클래스 객체)의 hashCode를 이용하면 됨.
+		return this.memberId.hashCode();
+	}
 }
